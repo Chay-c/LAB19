c9bfd73187cd36ad9303d5dbc7b9e47888e62114
Chay <chayangkul_c@cmu.ac.th>
2024-02-13 14:45:14 +0700

    tehe

--- lab19.h
+++ lab19.h
@@ -65,9 +65,115 @@ void Unit::newTurn(){
 
 
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+#include<iomanip>
+
+using namespace std;
+
+class Unit{
+		string name;
+		string type;		
+		int hp;
+		int hpmax;
+		int atk;
+		int def;
+		bool guard_on;		
+	public:			
+		void create(string);
+		void showStatus();
+		void newTurn();
+		int attack(Unit &);
+		int beAttacked(int);
+		int heal();	
+		void guard();
+		bool isDead();	
+};
+
+void Unit::create(string t){ 
+	if(t == "Hero"){
+		type = "Hero";
+		cout << "Please input your name: ";
+		getline(cin,name);
+		hpmax = rand()%20+90;
+		atk = rand()%5+14;
+		def = rand()%3+9;
+	}else if(t == "Monster"){
+		type = "Monster";
+		name = "Kraken";
+		hpmax = rand()%20+200;
+		atk = rand()%5+25;
+		def = rand()%3+5;
+	}
+	hp = hpmax;
+	guard_on = false;
+}
+
+void Unit::showStatus(){
+	if(type == "Hero"){
+		cout << "---------------------------------------\n"; 
+		cout << name << "\n"; 
+		cout << "HP: " << hp << "/" << hpmax << "\tATK: "<< atk << "\t\tDEF: "<< def;		
+		cout << "\n---------------------------------------\n";
+	}	
+	else if(type == "Monster"){
+		cout << "\t\t\t\t---------------------------------------\n"; 
+		cout << "\t\t\t\t" << name << "\n"; 
+		cout << "\t\t\t\tHP: " << hp << "\t\tATK: "<< atk << "\t\tDEF: "<< def;
+		cout << "\n\t\t\t\t---------------------------------------\n";
+	}
+}
+
+void Unit::newTurn(){
+	guard_on = false;
+}
+
+
+bool Unit::isDead(){
+  	if( hp <= 0 ) 
+	    return true ;
+	else{
+	    return false ;
+	}
+}
+
+
+void Unit::guard(){
+	guard_on=true ;
+}
+
+int Unit::beAttacked(int opatk){
+ int opdmg;
+    if(guard_on==false){
+         opdmg = opatk-def;
+    }
+    else{
+        opdmg= (opatk-def)/3;
+    }
+    hp=hp-opdmg;
+    return opdmg;
+}
+
+int Unit::attack(Unit &name){
+	return name.beAttacked(atk) ;
+}
+
+int Unit::heal(){
+	int toheal = rand()%21+10 ;
+	if((hp + toheal) >= hpmax){
+	    toheal=hpmax-hp ;
+	}
+	else if( hp == hpmax ){
+	    toheal = 0 ;
+	}
+	hp += toheal ;
+	return toheal ;
+}
+
+
+
 
 
 
